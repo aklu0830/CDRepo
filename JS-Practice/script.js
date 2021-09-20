d5b631253928e281d358fc4d4539a373db7fb1a1
Alex Klusa <aklusa71@gmail.com>
2021-09-20 13:02:07 -0500

    backup

---
+++ JS-Practice/script.js
@@ -0,0 +1,12 @@
+function messager(element) {
+    var swap = false;
+    if (swap === false) {
+        element.innerText = "Off";
+        swap === true;
+    } else if (swap === true) {
+        element.innerText === "On";
+        swap === false;
+    }
+}
+
+
