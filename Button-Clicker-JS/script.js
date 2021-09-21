2fcd7cf16fe73904983b32dc5ec5dc94dd7737dd
Alex Klusa <aklusa71@gmail.com>
2021-09-21 15:06:26 -0500

    UPP

---
+++ Button-Clicker-JS/script.js
@@ -0,0 +1,15 @@
+function login(el) {
+    el.innerText = "Logout";
+
+}
+
+function like(el) {
+    var element = el;
+    element.alert('Turd');
+
+}
+
+function rmbutton(el) {
+    var element = el;
+    element.remove();
+}
