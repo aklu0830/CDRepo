866eb6968eef937a8493fc341394a84d2d84660b
Alex Klusa <aklusa71@gmail.com>
2021-09-22 05:16:25 -0500

    liking-likes

---
+++ Liking-Likes/script.js
@@ -0,0 +1,20 @@
+var likes = document.querySelector("ctss");
+var likes_1 = 0;
+var likes_2 = 0;
+var likes_3 = 0;
+function like_1() {
+    likes_1++;
+    likes.innerText = likes + " likes(s)";
+}
+
+function like_2() {
+    likes_2++;
+    likes.innerText = likes + " likes(s)";
+
+}
+
+function like_3() {
+    likes_3++;
+    likes.innerText = likes + " likes(s)";
+
+}
