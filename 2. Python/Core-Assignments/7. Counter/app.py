e6b03e766f7526f84d8d8924ab1b41834456b788
Alex Klusa <aklusa71@gmail.com>
2021-09-30 11:28:45 -0500

    Counter

---
+++ 2. Python/Core-Assignments/7. Counter/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'me.webdevelopment.sucksatcareer'
+
+@app.route('/')
+def index():  # put application's code here
+    session['count'] = 0
+    return render_template('index.html', sessionc=session['count'])
+
+
+@app.route('/count', methods=['POST'])
+def submit():
+    session['count'] += 1
+    return render_template('index.html', sessionc=session['count'])
+
+@app.route('/count', methods=['GET'])
+def submittwo():
+    session['count'] += 1
+    return render_template('index.html', sessionc=session['count'])
+
+@app.route('/reset', methods=['GET'])
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+
+
+if __name__ == '__main__':
+    app.run()
