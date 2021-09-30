1c9ff21b70deb01fb34d83417869589678f7c2df
Alex Klusa <aklusa71@gmail.com>
2021-09-30 11:55:37 -0500

    Update

--- 2. Python/Core-Assignments/7. Counter/app.py
+++ 2. Python/Core-Assignments/7. Counter/app.py
@@ -3,28 +3,27 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'me.webdevelopment.sucksatcareer'
 
+
+
 @app.route('/')
 def index():  # put application's code here
-    session['count'] = 0
+    if 'count' in session:
+        session['count'] += 1
+    else:
+        session['count'] = 1
     return render_template('index.html', sessionc=session['count'])
 
 
-@app.route('/count', methods=['POST'])
+@app.route('/count', methods=['POST', 'GET'])
 def submit():
-    session['count'] += 1
-    return render_template('index.html', sessionc=session['count'])
+    
+    return redirect('/')
 
-@app.route('/count', methods=['GET'])
-def submittwo():
-    session['count'] += 1
-    return render_template('index.html', sessionc=session['count'])
 
 @app.route('/reset', methods=['GET'])
 def reset():
     session.clear()
     return redirect('/')
-
-
 
 
 if __name__ == '__main__':
