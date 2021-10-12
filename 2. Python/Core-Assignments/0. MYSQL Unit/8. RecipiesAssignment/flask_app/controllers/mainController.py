a82dd47133188fe819136485ae51f1cc354010c5
aklusa <aklusa71@gmail.com>
2021-10-12 17:30:38 -0500

    Recipes Backup

--- 2. Python/Core-Assignments/0. MYSQL Unit/8. RecipiesAssignment/flask_app/controllers/mainController.py
+++ 2. Python/Core-Assignments/0. MYSQL Unit/8. RecipiesAssignment/flask_app/controllers/mainController.py
@@ -11,6 +11,11 @@ def mainpage():
         session['loggedIN'] = False
         session['user_id'] = ""
         session['first_name'] = ""
+        session['editing'] = False
+        session['recid'] = ''
+
+    if session['loggedIN']:
+        return redirect('/dashboard')
 
     return render_template('login.html')
 
@@ -53,6 +58,7 @@ def logout():
 @app.route("/dashboard")
 def dashboard():
     data = recipe.Recipe.get_all()
+    session['editing'] = False
 
     if not session['loggedIN']:
         return render_template('404.html')
@@ -67,13 +73,81 @@ def newRecipePage():
 
 @app.route('/createrecipe', methods=['GET', 'POST'])
 def makerecipe():
-    data = {'name': request.form['name'], 'description': request.form['description'],
+    data = {'name': request.form['recipe_name'], 'description': request.form['description'],
             'instructions': request.form['instructions'], 'date_made': request.form['date'],
-            'under30': request.form['under30'],
-            'user_id': request.form['user_id']}
+            'under30': request.form['underthirty'],
+            'user_id': session['user_id']}
     recipe.Recipe.createrecipe(data)
 
+    return redirect('/dashboard')
+
+
+@app.route('/show/<int:recipe_id>')
+def showrecipe(recipe_id):
+    data = {'recipeid': recipe_id}
+
+    rec = recipe.Recipe.getrecipe(data)
+
+    return render_template('showrecipe.html', rec=rec)
+
+
+@app.route('/delete', methods=['GET', 'POST'])
+def removeRecipe():
+    data = {'recipeid': request.form['delete']}
+
+    recipe.Recipe.droprecipe(data)
+
+    return redirect('/dashboard')
+
+
+@app.route('/toggleEdit', methods=['GET', 'POST'])
+def toggleEditStatus():
+    if not session['editing']:
+        session['editing'] = True
+        recid = request.form['edit']
+        return redirect('/edit/' + recid)
+    else:
+        session['recid'] = ""
+        session['editing'] = False
+        return redirect('dashboard')
+
+
+@app.route('/edit')
+def edit1():
     return redirect('/')
+
+
+@app.route('/edit/')
+def edit2():
+    return redirect('/')
+
+
+@app.route('/edit/<int:recipeid>', methods=['GET', 'POST'])
+def editRecipe(recipeid):
+    if not session['editing']:
+        return redirect('/dashboard')
+
+    data = {'recipeid': recipeid}
+    session['recid'] = data['recipeid']
+    print(f"REC ID IS {session['recid']}")
+
+    rec = recipe.Recipe.getrecipe(data)
+
+    return render_template('editrecipe.html', rec=rec)
+
+
+@app.route('/applychanges', methods=['POST', 'GET'])
+def applychanges():
+    data = {'name': request.form['recipe_name'], 'description': request.form['description'],
+            'instructions': request.form['instructions'], 'under30': request.form['underthirty'],
+            'date_made': request.form['date'], 'recipeid': session['recid']}
+    recid = data['recipeid']
+    if not recipe.Recipe.validations(data):
+        return redirect(f'/edit/{recid}')
+
+    recipe.Recipe.updaterecipe(data)
+
+    return redirect('/dashboard')
 
 
 if __name__ == '__main__':
--- 2. Python/Core-Assignments/0. MYSQL Unit/8. RecipiesAssignment/flask_app/models/recipe.py
+++ 2. Python/Core-Assignments/0. MYSQL Unit/8. RecipiesAssignment/flask_app/models/recipe.py
@@ -43,8 +43,7 @@ class Recipe:
 
     @classmethod
     def createrecipe(self, data):
-
-        query = 'insert into recipes(name, description, instructions, under30, created_at, updated_at, user_id) values(%(name)s, %(description)s,%(instructions)s,%(under30)s,curdate(), curdate(), %(user_id)s;'
+        query = 'insert into recipes(`name`, `description`, `instructions`, `under30`, `created_at`, `updated_at`, `user_id`) values(%(name)s, %(description)s,%(instructions)s,%(under30)s,%(date_made)s, curdate(), %(user_id)s);'
 
         send = connectToMySQL(dbname).query_db(query, data)
 
@@ -52,7 +51,7 @@ class Recipe:
 
     @classmethod
     def droprecipe(self, data):
-        query = "delete from recipes where id=%(id)s;"
+        query = "delete from recipes where id=%(recipeid)s;"
 
         send = connectToMySQL(dbname).query_db(query, data)
 
@@ -60,8 +59,32 @@ class Recipe:
 
     @classmethod
     def getrecipe(cls, data):
-        query = "select * from recipes where id=%(email)s;"
+        query = "select * from recipes where id=%(recipeid)s;"
 
         send = connectToMySQL(dbname).query_db(query, data)
 
-        return send[0]
+        if len(send) < 1:
+            pass
+        else:
+
+            return send[0]
+
+    @classmethod
+    def updaterecipe(cls, data):
+        query = "update recipes set `name`=%(name)s, `description`=%(description)s, `instructions`=%(instructions)s, `under30`=%(under30)s, `created_at`=%(date_made)s, `updated_at`=now() where id=%(recipeid)s;"
+
+        send = connectToMySQL(dbname).query_db(query, data)
+
+        return send
+
+    @staticmethod
+    def validations(data):
+        is_valid = True
+        if len(data['name']) < 1:
+            flash(u'Name field cannot be empty', 'col-1')
+            is_valid = False
+        if len(data['description']) < 5:
+            flash(u'Description must be at least 5 characters', 'col-1')
+            is_valid = False
+        
+        return is_valid
