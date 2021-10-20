ed5a6ec915b98cd09b2a8a693969e39aed206470
aklusa <aklusa71@gmail.com>
2021-10-20 12:12:10 -0500

    Update

--- Projects/License/flask_app/controllers/mainController.py
+++ Projects/License/flask_app/controllers/mainController.py
@@ -5,23 +5,36 @@ from flask_app.models import user, apikeys
 
 @app.route("/")
 def mainPage():
-
-    if len(session)==0:
-        return "Welcome"
+    if session['logged_in'] in session:
+        return render_template("homepage.html")
     else:
-        session['logged_in'] = True
+        session['logged_in'] = False
         session['user_id'] = ""
         session['license_id'] = ""
         session['api_id'] = ""
-    return render_template("homepage.html")
+        return render_template("homepage.html")
+
 
 @app.route("/register")
 def register():
     return render_template("registration.html")
 
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session ['logged_in'] = False
+    return redirect("/")
+
+
+@app.route("/processlogin", methods=['GET', 'POST'])
+def processlogin():
+    session['logged_in'] = True
+    return redirect("/dashboard")
+
 
 @app.route("/dashboard")
 def dashboard():
@@ -30,6 +43,13 @@ def dashboard():
         return redirect("/login")
     else:
         return render_template("dashboard.html", apilist=apilist)
+
+@app.route("/makeproduct")
+def makeproduct():
+    if not session['logged_in']:
+        return redirect("/login")
+    else:
+        return redirect("/dashboard")
 
 
 @app.errorhandler(404)
