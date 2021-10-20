16b7a2a824d8a77bcb2065b29a8b6b5fc5df8c88
aklusa <aklusa71@gmail.com>
2021-10-19 20:40:57 -0500

    Update

--- Projects/License/flask_app/controllers/mainController.py
+++ Projects/License/flask_app/controllers/mainController.py
@@ -16,6 +16,15 @@ def mainPage():
 
     return "Main Page"
 
+@app.route("/register")
+def register():
+    return render_template("registration.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
