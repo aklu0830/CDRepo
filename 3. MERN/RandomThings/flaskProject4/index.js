6bc7da4ea855dcafa156dbe67969688686a4976d
aklusa <aklusa71@gmail.com>
2021-10-26 18:56:51 -0500

    Upppy Uppy

---
+++ 3. MERN/RandomThings/flaskProject4/index.js
@@ -0,0 +1,38 @@
+const pokemon = Object.freeze([
+    { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+    { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+    { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+    { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+    { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+    { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+    { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+    { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+    { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+    { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+    { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+    { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+    { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+    { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+    { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+    { "id": 98,  "name": "Krabby",     "types": ["water"] },
+    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+    { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+    { "id": 133, "name": "Eevee",      "types": ["normal"] },
+    { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+    { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+    { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+    { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+]);
+var count = 0
+const pkmnIdsDivThree = pokemon.filter( p => p.id%3===0)
+const pkmnIdsFire = pokemon.filter(p => p.types.includes('fire'))
+const pkmnIdsLen = pokemon.filter(p => p.types.length > 1)
+const pkmnIdsName = pokemon.filter(p => p.name).map(p => p.name)
+const pkmnIdGThan99 = pokemon.filter(p => p.id > 99)
+const pkmnIdWrPisn = pokemon.filter(p => p.types[0] === "poison" && p.types.length ===1)
+const pkmnIdWrL22AisFlng = pokemon.filter(p => p.types.length > 1 && p.types[1] === "flying")
+const pkmnIdsNormal = pokemon.filter(p => p.types.includes('normal')).length
+
+
+
+console.log(pkmnIdsNormal)
