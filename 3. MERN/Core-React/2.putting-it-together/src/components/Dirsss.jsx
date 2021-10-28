02ae5f94037ca32194af61dc2e0134572a578b8d
aklusa <aklusa71@gmail.com>
2021-10-28 15:27:27 -0500

    Update

--- 3. MERN/Core-React/2.putting-it-together/src/components/Dirsss.jsx
+++ 3. MERN/Core-React/2.putting-it-together/src/components/Dirsss.jsx
@@ -21,7 +21,7 @@ class Dirsss extends Component {
                 <h1>{this.props.lname}, {this.props.fname}</h1>
                 <h6>Age: { this.state.age}</h6>
                 <h6>Hair Color: {this.state.hclr}</h6>
-                <button onClick={this.incAge} className={"btn btn-primary"}>Flip Switch</button>
+                <button onClick={this.incAge} className={"btn btn-primary"}>Birthday Button For {this.props.fname} {this.props.lname}</button>
             </fieldset>
         );
     }
