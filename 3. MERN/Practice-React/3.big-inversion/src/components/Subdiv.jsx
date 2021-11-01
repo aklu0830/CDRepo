c7a7d206436ee322c7d996e659ddb086a0a5aa22
aklusa <aklusa71@gmail.com>
2021-11-01 13:34:38 -0500

    Uplolad

--- 3. MERN/Practice-React/3.big-inversion/src/components/Subdiv.jsx
+++ 3. MERN/Practice-React/3.big-inversion/src/components/Subdiv.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Subdiv = props => {
+    const [state, setState] = useState({
+        age: props.age
+    })
 
-
-
+    const clickHander = e => {
+        console.log(state)
+        setState({
+            age: state.age+1
+        })
+    }
 
     return(
       <div>
           <h1>{ props.lastname }, { props.firstname }</h1>
-          <p>Age: { props.age }</p>
+          <p>Age: { state.age }</p>
           <p>Hair Color: { props.hairColor }</p>
-
+          <button onClick={clickHander} className={"btn btn-primary"}>Increase Age</button>
       </div>
     );
 
