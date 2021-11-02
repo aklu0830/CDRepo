35962c4326de7ecaa9b0eff8fc02bd6dae5d9cb8
aklusa <aklusa71@gmail.com>
2021-11-02 11:42:57 -0500

    Update

---
+++ 3. MERN/Core-React/3.more-forms/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HookForm from "./components/HookForm";
+import MoreForms from "./components/HookForm";
+
+function App() {
+  return (
+    <div className="App labelss">
+        <MoreForms></MoreForms>
+    </div>
+  );
+}
+
+export default App;
