ece1c4cc781dbde0f959f5b267a32ff4eb2081cb
aklusa <aklusa71@gmail.com>
2021-11-12 15:33:35 -0600

    Update

--- 3. MERN/Exam/server/models/pet.model.js
+++ 3. MERN/Exam/server/models/pet.model.js
@@ -3,11 +3,17 @@ const mongoose = require('mongoose');
 const PetSchema = new mongoose.Schema({
     petType: {
         type: String,
-        minlength: [3, 'Must be atleast 3 characters'],
+        minlength: [3, 'Must be at least 3 characters'],
         required: [true, 'Field is required']
     },
-    petName: {type: String, minlength:3, required:true},
-    petDescription: {type: String, minlength:3, required:true},
+    petName: {type: String,
+        minlength: [3, 'Pet name must be at least 3 characters'],
+        required: [true, 'Pet name is required']
+    },
+    petDescription: {type: String,
+        minlength: [3, 'Description must be at least 3 characters'],
+        required: [true, 'Description is required']
+    },
     skillOne: {type: String},
     skillTwo: {type: String},
     skillThree: {type: String}
