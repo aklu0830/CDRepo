49f572b275b48dea006dfa37d7291d897a2b1a9a
aklusa <aklusa71@gmail.com>
2021-11-12 13:31:55 -0600

    Moar

--- 3. MERN/Exam/server.js
+++ 3. MERN/Exam/server.js
@@ -9,11 +9,9 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-const productRouteRegistration = require('./server/routes/product.routes');
 const petRouteRegistration = require('./server/routes/pet.routes');
 
 petRouteRegistration(app)
-productRouteRegistration(app)
 
 
 
--- 3. MERN/Exam/server/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const ProductController = require('../controllers/product.controller')
-
-module.exports = app => {
-    app.get('/api/products', ProductController.getAllProducts)
-    app.get('/api/products/:id', ProductController.getProduct)
-    app.post('/api/products/create', ProductController.createProduct)
-    app.put('/api/products/update/:id', ProductController.updateProduct)
-    app.delete('/api/products/delete/:id', ProductController.deleteProduct)
-}
