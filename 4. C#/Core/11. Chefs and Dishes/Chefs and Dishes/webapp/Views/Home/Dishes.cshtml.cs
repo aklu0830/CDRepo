d0759c99bd9e161057691dcd7482b4f0146754f4
Alex Klusa <aklusa71@gmail.com>
2021-12-16 14:33:35 -0600

    Update

---
+++ 4. C#/Core/11. Chefs and Dishes/Chefs and Dishes/webapp/Views/Home/Dishes.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Views.Home {
+    public class Dishes : PageModel {
+        public void OnGet() {
+            
+        }
+    }
+}
---
+++ 4. C#/Core/11. Chefs and Dishes/Chefs and Dishes/webapp/Views/Home/NewDish.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Views.Home {
+    public class NewDish : PageModel {
+        public void OnGet() {
+            
+        }
+    }
+}
