fc851626d8e8be07eb075c6e3ec69fa8781991bd
Alex Klusa <aklusa71@gmail.com>
2021-12-17 03:21:45 -0600

    Update

--- 4. C#/Core/12.Wedding Planner/Wedding Planner/webapp/Controllers/HomeController.cs
+++ 4. C#/Core/12.Wedding Planner/Wedding Planner/webapp/Controllers/HomeController.cs
@@ -49,7 +49,7 @@ namespace webapp.Controllers {
                 return RedirectToAction("Login");
             }
 
-            ViewBag.LoggedInUser = HttpContext.Session.GetInt32("UserID");
+            ViewBag.LoggedInUser = _context.Users.FirstOrDefault(d => d.UserId == HttpContext.Session.GetInt32("UserID"));
             ViewBag.Weddings = _context.Weddings.Include(g=>g.Guests).ToList();
             
             return View();
